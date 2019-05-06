6d59592e852c73eb536fbc09554be77fa2602233
Claudia Gutierrez <gutierrez.cp21@yahoo.com>
2019-05-06 02:31:29 -0400

    content

--- liri.js
+++ liri.js
@@ -1,43 +1,54 @@
 var axios = require("axios");
 var fs = require("fs");
-var Spotify = require('node-spotify-api');
-
+var spotify = require('node-spotify-api');
+var db = require("db");
 var spotifyone = new Spotify({
-    id: "2beb30788e6a4587b4c41d25d4a3d067",
-    secret: "fe1a632d0f97415e87c7fbdbb479dace"
+    id: process.env.ID,
+    secret: process.env.SECRET,
 });
+var spotify = spotify(options);
+inquirer.prompt([
 
-var spotify = function () {
-    this.findSong = function (term) {
-        var URL = 'https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx' + term;
-
-        axios.get(URL).then(
-            function (response) {
-                var result = response.data
-                console.log(result);
-                console.log(response);
-                var resultString = `Name: ${data.name}`;
-
-                // .catch (function (err) {
-                //         console.error('Error occurred: ' + err);
-
-
-                fs.appendFile("log.txt", 'Song:' + resultString, function (err) {
-                    if (err) {
-                        return err;
-                    }
-                    else {
-                        console.log(resultString);
-                        console.log("File was logged")
-                    }
-                }
-
-                );
-            }
-        );
-
+    {
+        type: "input",
+        name: "userInput",
+        message: "Which location or landmark would you like to geocode?"
     }
-};
+
+    // After the prompt, store the user's response in a variable called location.
+]).then(function (search) {
 
 
-module.exports = spotify;
+    // var spotify = function () {
+    //     this.findSong = function (term) {
+    //         var URL = 'https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx' + term;
+
+    //         axios.get(URL).then(
+    //             function (response) {
+    spotify.music(search.userInput, function (err, data) {
+        var result = response.data;
+        var resultString = `Name: ${data.name}`;
+
+        console.log(JSON.stringify(data, null, 2));
+        
+        //                 console.log(result);
+        //                 console.log(response);
+        
+
+        // .catch (function (err) {
+        //         console.error('Error occurred: ' + err);
+
+
+        fs.appendFile("log.txt", 'Song:' + resultString, function (err) {
+            if (err) {
+                return err;
+            }
+            else {
+                console.log(resultString);
+                console.log("File was logged")
+            }
+        }
+
+        )
+
+    });
